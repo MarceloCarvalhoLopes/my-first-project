b1ccdb1170e182d8310607fe83dce899534aef59
MarceloCarvalhoLopes <marcalolcarvalho@gmail.com>
2017-11-10 19:42:31 -0200

    Main file changed

--- src/main/java/com/smartsoft/api/MyFirstProjectApplication.java
+++ src/main/java/com/smartsoft/api/MyFirstProjectApplication.java
@@ -8,5 +8,6 @@ public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
+		System.out.println("My firs project !");
 	}
 }
